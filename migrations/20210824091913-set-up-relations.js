97c8d8a288db121970f45547db91f023f6640a39
andre2610 <dre2610@gmail.com>
2021-08-24 11:30:04 +0200

    created models and relations

---
+++ migrations/20210824091913-set-up-relations.js
@@ -0,0 +1,31 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn("players", "teamId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "teams",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn("players", "teamId");
+  },
+};
