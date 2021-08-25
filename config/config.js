080c50b8d4a934e012f797aa49c624e546d2e2f9
andre2610 <dre2610@gmail.com>
2021-08-25 17:17:29 +0200

    added postgres url to .env

---
+++ config/config.js
@@ -0,0 +1,22 @@
+const { POSTGRES_URL } = require("./constants");
+
+module.exports = {
+  development: {
+    url: POSTGRES_URL,
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
--- config/constants.js
+++ config/constants.js
@@ -5,4 +5,7 @@ module.exports = {
   SALT_ROUNDS: parseInt(process.env.SALT_ROUNDS) || 12,
   TOKEN_SECRET:
     process.env.TOKEN_SECRET || "e9rp^&^*&@9sejg)DSUA)jpfds8394jdsfn,m",
+  POSTGRES_URL:
+    process.env.POSTGRES_URL ||
+    "postgres://ywretvxu:pSOiuz0119aVuNe5ac8bBTtowqH9xlsO@tai.db.elephantsql.com/ywretvxu",
 };
