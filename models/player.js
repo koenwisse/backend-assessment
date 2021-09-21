5a2d43474739cb5b0fa9030998624f5dc1979edd
andre2610 <dre2610@gmail.com>
2021-09-21 11:46:49 +0200

    added profile picture to players model and to seeds

--- models/player.js
+++ models/player.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
       name: { type: DataTypes.STRING, allowNull: false },
       age: { type: DataTypes.INTEGER, allowNull: false },
       nationality: { type: DataTypes.STRING, allowNull: false },
-      profilePicture: { type: DataTypes.STRING },
+      profilePicture: { type: DataTypes.TEXT },
     },
     {
       sequelize,
