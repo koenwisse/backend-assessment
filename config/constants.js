4b6747bdfa2ed3af95ac4407d83ac7ed5a7f3fac
andre2610 <dre2610@gmail.com>
2021-09-28 19:19:09 +0200

    created ProcFile and postRelease.sh

--- config/constants.js
+++ config/constants.js
@@ -3,9 +3,6 @@ require("dotenv").config();
 module.exports = {
   PORT: process.env.PORT || 4000,
   SALT_ROUNDS: parseInt(process.env.SALT_ROUNDS) || 12,
-  TOKEN_SECRET:
-    process.env.TOKEN_SECRET || "e9rp^&^*&@9sejg)DSUA)jpfds8394jdsfn,m",
-  POSTGRES_URL:
-    process.env.POSTGRES_URL ||
-    "postgres://ywretvxu:pSOiuz0119aVuNe5ac8bBTtowqH9xlsO@tai.db.elephantsql.com/ywretvxu",
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
+  POSTGRES_URL: process.env.POSTGRES_URL,
 };
