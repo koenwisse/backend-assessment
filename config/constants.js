da6cbf870e0919fa22e9418144f87ea1544d61b8
Andre Silva <dre2610@gmail.com>
2021-10-11 12:21:38 +0200

    Add fallback value for token secret

--- config/constants.js
+++ config/constants.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 module.exports = {
   PORT: process.env.PORT || 4000,
   SALT_ROUNDS: parseInt(process.env.SALT_ROUNDS) || 12,
-  TOKEN_SECRET: process.env.TOKEN_SECRET,
+  TOKEN_SECRET: process.env.TOKEN_SECRET || "jahgdjhavflhavfjkhasvlhadaad",
   POSTGRES_URL: process.env.POSTGRES_URL,
 };
